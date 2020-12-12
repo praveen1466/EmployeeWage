cc76a788833706c068c9a17fa8f6e1f9262e03d3
praveen1466 <praveensknl7@gmail.com>
2020-12-12 16:37:01 +0530

    Added: Calculating total wage using Array forEach and reduce method

--- employeeWage.js
+++ employeeWage.js
@@ -40,3 +40,23 @@ function getWorkingHours(empCheck)
 let empWage = calcDailyWage(totalEmpHrs);
 console.log("UC6 - Total Days: " +totalWorkingDays+ 
 	" Total Hrs: " +totalEmpHrs+ " Employee Wage " + empWage);
+
+//Array Helper Functions
+//UC 7A Calc total Wage using Array forEach traversal or reduce method
+let totEmpWage = 0;
+function sum(dailyWage)
+{
+	totEmpWage = totEmpWage + dailyWage;
+}
+
+empDailyWageArr.forEach(sum)
+{
+	console.log("UC7A - Total Days " + totalWorkingDays +
+		" Total Hrs: " + totalEmpHrs + " Emp Wage :" + totEmpWage);
+}
+
+function totalWages(totalWage, dailyWage)
+{
+	return totalWage + dailyWage;
+}
+console.log("UC7A - Employee Wage with reduce: " + empDailyWageArr.reduce(totalWages, 0));
