7c01ae1b979243f09143b000929f8e8d9033184a
praveen1466 <praveensknl7@gmail.com>
2020-12-15 21:52:20 +0530

    Added: employee payroll data to store gender and start date

--- EmployeePayRollData.js
+++ EmployeePayRollData.js
@@ -3,27 +3,35 @@ class EmployeePayRollData{
     id;
     name;
     salary;
+    gender;
+    startDate;
 
 
-//constructor
-constructor(id,name,salary) {
-    this.id=id;
-    this.name=name;
-    this.salary=salary;
-} 
+    //constructor
+    constructor(...params) {
+        this.id=params[0];
+        this.name=params[1];
+        this.salary=params[2];
+        this.gender=params[3];
+        this.startDate=params[4];
+    } 
 
-//getter and setter method
-get name(){
-return this.name;
-}
-set name(name){
-    this._name=name;
-}
+    //getter and setter method
+    get name(){
+    return this.name;
+    }
+    set name(name){
+        this._name=name;
+    }
 
-//method
-toString(){
-    return "id=" + this.id + ", name=" + this.name + ", salary=" +this.salary;
-}
+    //method
+    toString(){
+        const options = { year: 'numeric', month: 'long', day: 'numeric'};
+        const empDate = this.startDate === undefined ? "undefined" :
+                        this.startDate.toLocaleDateString("en-US", options);
+        return "id=" + this.id + ", name=" + this.name + ", salary=" + this.salary + ", "+
+                "gender=" + this.gender +", startDate=" +empDate;
+    }
 
 }
 
@@ -31,3 +39,5 @@ let employeePayRollData = new EmployeePayRollData(1, "Praveen", 30000);
 console.log(employeePayRollData.toString());
 employeePayRollData.name="Raju";
 console.log(employeePayRollData.toString());
+let newEmployeePayrollData = new EmployeePayRollData(1, "harini", 30000, "F", new Date());
+console.log(newEmployeePayrollData.toString());
